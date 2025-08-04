3d15df73c7b77ca008d63f91b65373202589bf87
aalopezb <a.alexander.l.b@gmail.com>
2025-08-04 14:50:58 -0500

    feat: :fire: Changes in Back for completed the Front

--- microservicios/reservas/index.js
+++ microservicios/reservas/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3030;
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 let reservas = [];
 
